b61b371f943bafb580a2d705e9f8f43e6463116f
Daniel Do <do2daniel@yahoo.com>
2024-05-22 04:42:56 -0700

    Initial commit

---
+++ src/Scenes/Controls.js
@@ -0,0 +1,42 @@
+class Controls extends Phaser.Scene {
+    constructor() {
+        super("controlsScene");
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        // menu text configuration
+        let menuConfig = {
+            fontFamily: 'Arial',
+            fontSize: '28px',
+            backgroundColor: '#A020F0',
+            color: '#FFFFFF',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        
+        // show menu text
+        this.add.text(game.config.width/2, game.config.height/2, 'Frosty Frenzy', menuConfig).setOrigin(1.25);
+        this.add.text(game.config.width/2 + 100, game.config.height/2, 'Controls', menuConfig).setOrigin(1.25);
+
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+   
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            this.scene.start("creditsScene");
+        }
+      }
+}
---
+++ src/main.js
@@ -0,0 +1,40 @@
+// Daniel Do
+// Created: 5/22/2024
+// Phaser: 3.70.0
+//
+// Frosty Frenzy (CMPM 120 Gallery Shooter Project)
+//
+// A snowball fight shoot em up game made with Phaser
+//
+// Uses code borrowed from Jim Whitehead
+// 
+// All assets from Kenny Assets:
+// Tiny Ski: https://kenney.nl/assets/tiny-ski
+// Rolling Ball Assets: https://kenney.nl/assets/rolling-ball-assets
+// Impact Sounds: https://kenney.nl/assets/impact-sounds
+// UI Audio: https://kenney.nl/assets/ui-audio
+// Music Jingles: https://kenney.nl/assets/music-jingles 
+// Interface Sounds: https://kenney.nl/assets/interface-sounds
+
+// debug with extreme prejudice
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    width: 960,
+    height: 640,
+    scene: [Menu, Controls, Credits, Play, Gameover]
+}
+
+// Global variable to hold sprites
+var my = {sprite: {}};
+let score = 0;
+
+const game = new Phaser.Game(config);
+
+let keyLEFT, keyRIGHT;
